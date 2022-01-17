25a36cb2826c4f4f684134ed58abf65b320cddc2
vaish <vas2021123@sicsr.ac.in>
2022-01-17 20:42:51 +0530

    contact us icon commit

---
+++ app/src/main/java/com/vgroup/flexfit/ContactusActivity.java
@@ -0,0 +1,14 @@
+package com.vgroup.flexfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactusActivity extends AppCompatActivity {
+    ImageView gmail,twitter,instagram;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contactus);
+    }
+}
