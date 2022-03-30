aab4a68b9ee7f80d4ce147ad14d4f6d22993a67e
vaish <vas2021123@sicsr.ac.in>
2022-03-30 16:24:00 +0530

    -aboutus update1 -contact pg update -recipe design 30/03/22

--- app/src/main/java/com/vgroup/flexfit/activities/activity_recipes.java
+++ app/src/main/java/com/vgroup/flexfit/activities/activity_recipes.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vgroup.flexfit.R;
 
@@ -14,7 +15,12 @@ TextView textView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
-        textView = findViewById(R.id.textViewLink);
+        textView = findViewById(R.id.watchVid);
+        try{
         textView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+        catch(Exception e){
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_LONG).show();
+        }
     }
 }
